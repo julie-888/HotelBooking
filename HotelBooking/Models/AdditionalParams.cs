702a50d84b6db5d4f606d432cdc1f23e9f44df7f
Julie <84780655+julie-888@users.noreply.github.com>
2023-03-13 22:51:08 +0600

    .

--- HotelBooking/Models/AdditionalParams.cs
+++ HotelBooking/Models/AdditionalParams.cs
@@ -2,7 +2,7 @@
 {
     public class AdditionalParams
     {
-        public int Id { get; set; }
+        public int Id { get; set; }  
         public string AParamPName { get; set; }
         public string AParamValue { get; set; }
     }
